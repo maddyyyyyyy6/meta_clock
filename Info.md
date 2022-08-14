b42fa655b42d4d266341d0564171e11729116f89
maddyyyyyyy6 <maddyyyyyyy6@gmail.com>
2022-08-14 22:27:39 +0530

    Hamburger menu fucntioning

--- Info.md
+++ Info.md
@@ -8,4 +8,11 @@ TODO List for Clock App
 *6.  Hamburger Menu
 *7.  Hamburger Menu Functioning
 *8.  Tabs in Hamburger Menu
-9.  ...
+*9.  Added Alarm Tab
+*10. Added Alarm Add Button
+*11. Added Alarm Add Menu
+*12. Timer Tab
+*13. Timer Controls
+*14. Stopwatch Controls UI and Design
+*15. Added Hamburger Funtioning
+
--- src/JS/Hamburber_Menu.js
+++ src/JS/Hamburber_Menu.js
@@ -4,3 +4,56 @@ const SideMenu = document.getElementById('side-menu');
 MenuButton.addEventListener('click',() => {
     SideMenu.classList.toggle('show');
 })
+
+const Clock_switch = document.getElementById('Clock-side-menu');
+const Alarm_switch = document.getElementById('Alarm-side-menu');
+const Timer_switch = document.getElementById('Timer-side-menu');
+const Stopwatch_switch = document.getElementById('Stopwatch-side-menu');
+
+
+const  Clock_View  = document.getElementById('Clock');
+const  Alarm_View  = document.getElementById('Alarm');
+const  Timer_View  = document.getElementById('Timer');
+const  Stopwatch_View  = document.getElementById('Stopwatch');
+
+Clock_switch.addEventListener('click',() => {
+    hideOtherTabs();
+    Clock_View.classList.remove('hidden');
+    MenuButton.click();
+    console.log('Clock_switch');
+    
+})
+
+Alarm_switch.addEventListener('click',() => {
+    hideOtherTabs();
+    Alarm_View.classList.remove('hidden');
+    MenuButton.click();
+    console.log('Alarm_switch');
+    
+})
+
+Timer_switch.addEventListener('click',() => {
+    hideOtherTabs();
+    Timer_View.classList.remove('hidden');
+    MenuButton.click();
+    console.log('Timer_switch');
+    
+})
+
+Stopwatch_switch.addEventListener('click',() => {
+    hideOtherTabs();
+    Stopwatch_View.classList.remove('hidden');
+    MenuButton.click();
+    console.log('Stopwatch_switch');
+    
+})
+
+
+
+function hideOtherTabs(){
+    Clock_View.classList.add('hidden');
+    Alarm_View.classList.add('hidden');
+    Timer_View.classList.add('hidden');
+    Stopwatch_View.classList.add('hidden');
+
+}
